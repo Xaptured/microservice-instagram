325fa2e1499eb4f83b6aa3066380e7bedaea2044
Jack <jk19011999@gmail.com>
2023-07-28 21:55:56 +0530

    Created the basic for the project

---
+++ src/test/java/com/thejackfolio/microservices/instagramapi/InstagramapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejackfolio.microservices.instagramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
